7cb270a652fe82898d1b02544631c246d57fde21
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-06 22:11:49 -0800

    Day 01

---
+++ src/test/java/net/projecteuler/Day01Spec.java
@@ -0,0 +1,31 @@
+package net.projecteuler;
+
+import org.junit.Test;
+
+public class Day01Spec {
+    /**
+     * Problem 1: Multiples of 3 and 5
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+     * The sum of these multiples is 23.
+     *
+     * Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+     */
+
+    @Test
+    public void givenInput1000Return233168(){
+    }
+
+    @Test
+    public void givenInput49Return543(){
+
+    }
+
+    @Test
+    public void givenInput19564Return89301183(){
+    }
+
+    @Test
+    public void givenInput8456Return16687353(){
+    }
+
+}
