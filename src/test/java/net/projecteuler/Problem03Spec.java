38dd4d129e7f5986e14b68b647420cc0cef4ee34
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-07 14:36:08 -0800

    Problem 03

---
+++ src/test/java/net/projecteuler/Problem03Spec.java
@@ -0,0 +1,38 @@
+package net.projecteuler;
+
+import org.junit.Test;
+
+/**
+ * Problem 3: Largest prime factor
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the given number?
+ */
+public class Problem03Spec {
+
+    // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-3-largest-prime-factor
+    @Test
+    public void givenInput_2Return2() {
+    }
+
+    @Test
+    public void givenInput_3Return3() {
+    }
+
+    @Test
+    public void givenInput_5Return5() {
+    }
+
+    @Test
+    public void givenInput_7Return7() {
+    }
+
+    @Test
+    public void givenInput_13195Return29() {
+    }
+
+    @Test
+    public void givenInput_600851475143Return6857() {
+    }
+
+}
