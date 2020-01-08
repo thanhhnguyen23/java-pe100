8fb0fb47d2178bd87432a0ccbfee02a27d39288e
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-08 09:31:09 -0800

    Problem 04

---
+++ src/test/java/net/projecteuler/Problem04Spec.java
@@ -0,0 +1,25 @@
+package net.projecteuler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Problem 4: Largest palindrome product
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from the product of two n-digit numbers.
+ */
+public class Problem04Spec {
+
+    // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-4-largest-palindrome-product
+    @Test
+    public void givenInput_2Return_9009() {
+        fail();
+    }
+
+    @Test
+    public void givenInput_3Return906609() {
+        fail();
+    }
+}
