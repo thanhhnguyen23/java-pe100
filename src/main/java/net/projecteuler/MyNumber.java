415907f74ebf0955fb6f008807a29ff23e63ef7e
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-08 09:18:50 -0800

    complete problem 3

---
+++ src/main/java/net/projecteuler/MyNumber.java
@@ -0,0 +1,107 @@
+package net.projecteuler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyNumber {
+
+    List<Long> primeFactors = new ArrayList<>();
+    List<Long> factors = new ArrayList<>();
+
+    private long input;
+
+    public List<Long> findFactors(long input) {
+        setInput(input);
+        findFractorsProcessor();
+        return factors;
+    }
+
+    private void findFractorsProcessor() {
+        if (smallInputs()) {
+            for (int i = 1; i <= getInput(); ++i) {
+                processingFactors(i);
+            }
+
+        } else {
+            for (int i = 1; i <= Math.sqrt(getInput()) + 1; ++i) {
+                processingFactors(i);
+            }
+        }
+        removeFirstElement();
+    }
+
+    private boolean smallInputs() {
+        return getInput() < 99999;
+    }
+
+    private void processingFactors(int i) {
+        if (getInput() % i == 0) {
+            factors.add((long) i);
+        }
+    }
+
+    private void removeFirstElement() {
+        if (factors.size() > 2) {
+            factors.remove(0);
+        }
+    }
+
+    public boolean isPrime(long primeNumber) {
+        int sqrt = (int) Math.sqrt(primeNumber) + 1;
+        return isPrimeProcessor(primeNumber, sqrt);
+    }
+
+    private boolean isPrimeProcessor(long primeNumber, int sqrt) {
+        for (int i = 2; i < sqrt; ++i) {
+            if (primeNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Long> findPrimeFactors(long input) {
+        setInput(input);
+        long factorsSize = findFactors(getInput()).size();
+
+        primeFactorsProcessor(factorsSize);
+
+//        System.out.println("prime factors: " + primeFactors);
+        return primeFactors;
+    }
+
+    private void primeFactorsProcessor(long factorsSize) {
+        for (int i = 0; i < factorsSize; ++i) {
+            if (isPrime(getFactors().get(i))) {
+                primeFactors.add(getFactors().get(i));
+            }
+        }
+        if (primeFactors.size() > 1) {
+            primeFactors.remove(0);
+        }
+    }
+
+    public Long findLargestPrimeFactor(long input) {
+        setInput(input);
+        findPrimeFactors(input);
+//        System.out.println("largest prime factor: " + Collections.max(getPrimeFactors()));
+        return Collections.max(getPrimeFactors());
+    }
+
+    public List<Long> getPrimeFactors() {
+        return this.primeFactors;
+    }
+
+    public List<Long> getFactors() {
+        return this.factors;
+    }
+
+    public long getInput() {
+        return this.input;
+    }
+
+    public void setInput(long input) {
+        this.input = input;
+    }
+}
--- src/test/java/net/projecteuler/Problem03Spec.java
+++ src/test/java/net/projecteuler/Problem03Spec.java
@@ -2,6 +2,11 @@ package net.projecteuler;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
 /**
  * Problem 3: Largest prime factor
  * The prime factors of 13195 are 5, 7, 13 and 29.
@@ -13,26 +18,63 @@ public class Problem03Spec {
     // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-3-largest-prime-factor
     @Test
     public void givenInput_2Return2() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(2, myNumber.findLargestPrimeFactor(2), 0.01);
     }
 
     @Test
     public void givenInput_3Return3() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(3, myNumber.findLargestPrimeFactor(3), 0.01);
     }
 
     @Test
     public void givenInput_5Return5() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(5, myNumber.findLargestPrimeFactor(5), 0.01);
     }
 
     @Test
     public void givenInput_7Return7() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(7L, myNumber.findLargestPrimeFactor(7), 0.01);
     }
 
     @Test
     public void givenInput_13195Return29() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(29L, myNumber.findLargestPrimeFactor(13195), 0.01);
     }
 
     @Test
     public void givenInput_600851475143Return6857() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(6857L, myNumber.findLargestPrimeFactor(600851475143L), 0.01);
+    }
+
+    // additional tests
+    @Test
+    public void givenInputReturnFactors() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(Arrays.asList(2L,3L,4L,6L,8L,12L,24L), myNumber.findFactors(24));
+    }
+
+    @Test
+    public void givenFactorsReturnPrime() {
+        MyNumber myNumber = new MyNumber();
+        assertTrue(myNumber.isPrime(2));
+    }
+
+    @Test
+    public void givenInputReturnPrimeFactors() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(Collections.singletonList(3L), myNumber.findPrimeFactors(24));
+    }
+
+    @Test
+    public void givenInputReturnLargestPrimeFactors() {
+        MyNumber myNumber = new MyNumber();
+        assertEquals(3, myNumber.findLargestPrimeFactor(24), 0.01);
     }
 
 }
