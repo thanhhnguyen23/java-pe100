db77338c09ba289a77887753ddc0ef66e4519d23
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-09 18:30:32 -0800

    Problem 05

---
+++ src/test/java/net/projecteuler/Problem05Spec.java
@@ -0,0 +1,36 @@
+package net.projecteuler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Problem 5: Smallest multiple
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ *
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+ */
+public class Problem05Spec {
+
+    // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-5-smallest-multiple
+    @Test
+    public void givenInput5_Return60() {
+        fail();
+    }
+
+    @Test
+    public void givenInput7_Return420() {
+        fail();
+    }
+
+    @Test
+    public void givenInput13_return360360() {
+        fail();
+    }
+
+    @Test
+    public void givenInput20_return232792560() {
+        fail();
+    }
+
+}
