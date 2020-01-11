4b76dfee50e9b23d428832c72ae466936a1bacd0
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-10 21:12:11 -0800

    Problem 06

---
+++ src/test/java/net/projecteuler/Problem06Spec.java
@@ -0,0 +1,37 @@
+package net.projecteuler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Problem 6: Sum square difference
+ * The sum of the squares of the first ten natural numbers is,
+ * 12 + 22 + ... + 102 = 385
+ *
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+ * 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first n natural numbers and the square of the sum.
+ */
+public class Problem06Spec {
+
+    // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-6-sum-square-difference
+
+    @Test
+    public void given10_Return2640(){
+        fail();
+    }
+    @Test
+    public void given20_Return41230(){
+        fail();
+    }
+
+    @Test
+    public void given100_Return25164150(){
+        fail();
+    }
+}
