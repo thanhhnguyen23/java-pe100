db85e803810b70ceeacb6aeed28323d5f59251a8
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-11 21:01:14 -0800

    complete problem 6

--- src/main/java/net/projecteuler/SumSquareDifference.java
+++ src/main/java/net/projecteuler/SumSquareDifference.java
@@ -1,4 +1,26 @@
 package net.projecteuler;
 
 public class SumSquareDifference {
+
+    public int sumOfSquaresOfNNumbers(int n) {
+        int sumSquare = 0;
+        for(int i = n; i > 0; i--){
+            sumSquare += (i * i);
+        }
+        return sumSquare;
+    }
+
+    public int squareOfSumOfNNumbers(int n) {
+        double squareSum = 0;
+        for(int i = n; i > 0; i--){
+            squareSum += i;
+        }
+        squareSum = Math.pow(squareSum,2);
+        return (int) squareSum;
+    }
+
+    public int difference(int n) {
+        int result = 0;
+        return squareOfSumOfNNumbers(n) - sumOfSquaresOfNNumbers(n);
+    }
 }
--- src/test/java/net/projecteuler/Problem06Spec.java
+++ src/test/java/net/projecteuler/Problem06Spec.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.fail;
  *
  * The square of the sum of the first ten natural numbers is,
  * (1 + 2 + ... + 10)2 = 552 = 3025
+ *
  * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
  * 3025 − 385 = 2640.
  *
@@ -20,18 +21,40 @@ import static org.junit.Assert.fail;
 public class Problem06Spec {
 
     // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-6-sum-square-difference
-
     @Test
-    public void given10_Return2640(){
-        fail();
-    }
-    @Test
-    public void given20_Return41230(){
-        fail();
+    public void given10_Return2640() {
+        SumSquareDifference ssd = new SumSquareDifference();
+        assertEquals(2640, ssd.difference(10));
     }
 
     @Test
-    public void given100_Return25164150(){
-        fail();
+    public void given20_Return41230() {
+        SumSquareDifference ssd = new SumSquareDifference();
+        assertEquals(41230, ssd.difference(20));
+    }
+
+    @Test
+    public void given100_Return25164150() {
+        SumSquareDifference ssd = new SumSquareDifference();
+        assertEquals(25164150, ssd.difference(100));
+    }
+
+    // additional tests
+    @Test
+    public void givenInput_ReturnSumOfSquaresOfTheFirstNNaturalNumbers() {
+        SumSquareDifference ssd = new SumSquareDifference();
+        assertEquals(385, ssd.sumOfSquaresOfNNumbers(10));
+    }
+
+    @Test
+    public void givenInput_ReturnSquareOfSumOfTheFirstNNumbers() {
+        SumSquareDifference ssd = new SumSquareDifference();
+        assertEquals(3025, ssd.squareOfSumOfNNumbers(10));
+    }
+
+    @Test
+    public void givenInput_ReturnSquareOfSum_Minus_sumOfSquares() {
+        SumSquareDifference ssd = new SumSquareDifference();
+        assertEquals(2640, ssd.difference(10));
     }
 }
