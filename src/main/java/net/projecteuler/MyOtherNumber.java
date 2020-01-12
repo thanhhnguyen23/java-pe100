70304d974564150e05c1ecd9e39ce142d1fdd5ae
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-12 00:46:24 -0800

    complete problem 7

--- src/main/java/net/projecteuler/MyOtherNumber.java
+++ src/main/java/net/projecteuler/MyOtherNumber.java
@@ -1,4 +1,36 @@
 package net.projecteuler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyOtherNumber {
+    List<Integer> primes = new ArrayList<>();
+    private long input;
+
+    public Integer findNthPrime(long n) {
+        this.input = n;
+
+        for (int i = 1; i < Math.pow(2, n); i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+                if (isNPrime(n)) break;
+            }
+        }
+        return primes.get(primes.size() - 1);
+    }
+
+    private boolean isNPrime(long n) {
+        return primes.size() > n;
+    }
+
+    public boolean isPrime(long n) {
+        int sqrt = (int) Math.sqrt(n) + 1;
+        for (int i = 2; i < sqrt; ++i) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/test/java/net/projecteuler/Problem07Spec.java
+++ src/test/java/net/projecteuler/Problem07Spec.java
@@ -2,37 +2,52 @@ package net.projecteuler;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Problem 7: 10001st prime
  * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
  *
  * What is the nth prime number?
- * 
  */
 public class Problem07Spec {
 
     // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-7-10001st-prime
     @Test
     public void given6_return13(){
-        fail();
+        MyOtherNumber myOtherNumber = new MyOtherNumber();
+        assertEquals(13, myOtherNumber.findNthPrime(6), 0.01);
     }
+
     @Test
     public void given10_return29(){
-        fail();
+        MyOtherNumber myOtherNumber = new MyOtherNumber();
+        assertEquals(29, myOtherNumber.findNthPrime(10), 0.01);
     }
+
     @Test
     public void given100_return541(){
-        fail();
+        MyOtherNumber myOtherNumber = new MyOtherNumber();
+        assertEquals(541, myOtherNumber.findNthPrime(100), 0.01);
     }
+
     @Test
     public void given1000_return7919(){
-        fail();
+        MyOtherNumber myOtherNumber = new MyOtherNumber();
+        assertEquals(7919, myOtherNumber.findNthPrime(1000), 0.01);
     }
+
     @Test
     public void given10001_return104743(){
-        fail();
+        MyOtherNumber myOtherNumber = new MyOtherNumber();
+        assertEquals(104743, myOtherNumber.findNthPrime(10001), 0.01);
+    }
+
+    // additional tests
+    @Test
+    public void given4_return7(){
+        MyOtherNumber myOtherNumber = new MyOtherNumber();
+        assertEquals(7, myOtherNumber.findNthPrime(4), 0.01);
     }
 
 }
