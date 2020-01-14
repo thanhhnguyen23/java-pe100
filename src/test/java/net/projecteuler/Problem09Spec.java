2f7be9c2864bdab3e0be21232d293d14f69105c2
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-13 20:29:53 -0800

    Problem 09

---
+++ src/test/java/net/projecteuler/Problem09Spec.java
@@ -0,0 +1,34 @@
+package net.projecteuler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Problem 9: Special Pythagorean triplet
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ * a^2 + b^2 = c^2
+ * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc such that a + b + c = n.
+ *
+ */
+
+public class Problem09Spec {
+
+    // original tests: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-9-special-pythagorean-triplet
+    @Test
+    public void given1000_Return31875000(){
+        fail();
+    }
+    @Test
+    public void given24_Return480(){
+        fail();
+    }
+    @Test
+    public void given120_Return49920_OR_55080_OR_60000(){
+        fail();
+    }
+
+}
